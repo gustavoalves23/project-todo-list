d201a3ef902bfd06bcd09ee3f82d49e56de57b8f
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2021-06-02 15:30:53 -0300

    Desafios 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const addTaskButton = document.getElementById('criar-tarefa');
 
+function setToSelectedItem(clickedItem) {
+  let item = clickedItem.target;
+  let listSize = document.getElementById('lista-tarefas').childElementCount;
+  for (let index = 0; index < listSize; index += 1) {
+    let listItem = document.getElementById('lista-tarefas').children;
+    listItem[index].className = 'list-item';
+  }
+  item.className = 'selected-list-item';
+}
+
+function addClickPropertieToItens() {
+  let listSize = document.getElementById('lista-tarefas').childElementCount;
+  for (let index = 0; index < listSize; index += 1) {
+    let listItem = document.getElementsByClassName('list-item');
+    listItem[index].addEventListener('click', setToSelectedItem);
+  }
+}
+
 function addTask() {
   const listTaskInput = document.getElementById('texto-tarefa').value;
   const lista = document.getElementById('lista-tarefas');
-  const listItem = document.createElement('li');
+  let listItem = document.createElement('li');
+  listItem.className = 'list-item';
   listItem.innerText = listTaskInput;
   lista.appendChild(listItem);
   document.getElementById('texto-tarefa').value = '';
+  addClickPropertieToItens();
 }
 addTaskButton.addEventListener('click', addTask);
